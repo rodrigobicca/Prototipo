5636eb8f9ace504272e20bd0994e678f9681753f
Bicca Rodrigo <rodrigobicca@yahoo.com.br>
2020-07-10 12:34:00 +0000

    Início

---
+++ webapp/controller/Page1.controller.js
@@ -0,0 +1,165 @@
+sap.ui.define(["sap/ui/core/mvc/Controller",
+	"sap/m/MessageBox",
+	"./utilities",
+	"sap/ui/core/routing/History"
+], function(BaseController, MessageBox, Utilities, History) {
+	"use strict";
+
+	return BaseController.extend("com.sap.build.standard.app.controller.Page1", {
+		handleRouteMatched: function(oEvent) {
+			var sAppId = "App5f00d559f571944c1f49ecff";
+
+			var oParams = {};
+
+			if (oEvent.mParameters.data.context) {
+				this.sContext = oEvent.mParameters.data.context;
+
+			} else {
+				if (this.getOwnerComponent().getComponentData()) {
+					var patternConvert = function(oParam) {
+						if (Object.keys(oParam).length !== 0) {
+							for (var prop in oParam) {
+								if (prop !== "sourcePrototype" && prop.includes("Set")) {
+									return prop + "(" + oParam[prop][0] + ")";
+								}
+							}
+						}
+					};
+
+					this.sContext = patternConvert(this.getOwnerComponent().getComponentData().startupParameters);
+
+				}
+			}
+
+			var oPath;
+
+			if (this.sContext) {
+				oPath = {
+					path: "/" + this.sContext,
+					parameters: oParams
+				};
+				this.getView().bindObject(oPath);
+			}
+
+		},
+		_onSearchFieldLiveChange: function(oEvent) {
+			var sControlId = "sap_Responsive_Page_0-content-sap_m_ObjectList-1593892578068";
+			var oControl = this.getView().byId(sControlId);
+
+			// Get the search query, regardless of the triggered event ('query' for the search event, 'newValue' for the liveChange one, 'value' for the liveChange of SelectDialogs).
+			var sQuery = oEvent.getParameter("query") || oEvent.getParameter("newValue") || oEvent.getParameter("value");
+			var sSourceId = oEvent.getSource().getId();
+
+			return new Promise(function(fnResolve) {
+				var aFinalFilters = [];
+
+				var aFilters = [];
+				// 1) Search filters (with OR)
+				if (sQuery && sQuery.length > 0) {
+
+					aFilters.push(new sap.ui.model.Filter("ID", sap.ui.model.FilterOperator.Contains, sQuery));
+
+					aFilters.push(new sap.ui.model.Filter("Nome", sap.ui.model.FilterOperator.Contains, sQuery));
+
+					aFilters.push(new sap.ui.model.Filter("Rua", sap.ui.model.FilterOperator.Contains, sQuery));
+
+					aFilters.push(new sap.ui.model.Filter("CEP", sap.ui.model.FilterOperator.Contains, sQuery));
+
+					aFilters.push(new sap.ui.model.Filter("Cidade", sap.ui.model.FilterOperator.Contains, sQuery));
+
+					aFilters.push(new sap.ui.model.Filter("Pais", sap.ui.model.FilterOperator.Contains, sQuery));
+
+				}
+
+				var aFinalFilters = aFilters.length > 0 ? [new sap.ui.model.Filter(aFilters, false)] : [];
+				var oBindingOptions = this.updateBindingOptions(sControlId, {
+					filters: aFinalFilters
+				}, sSourceId);
+				var oBindingInfo = oControl.getBindingInfo("items");
+				if (oBindingInfo) {
+					oControl.bindAggregation("items", {
+						model: oBindingInfo.model,
+						path: oBindingInfo.path,
+						parameters: oBindingInfo.parameters,
+						template: oBindingInfo.template,
+						templateShareable: true,
+						sorter: oBindingOptions.sorters,
+						filters: oBindingOptions.filters
+					});
+				}
+			}.bind(this)).catch(function(err) {
+				if (err !== undefined) {
+					MessageBox.error(err.message);
+				}
+			});
+
+		},
+		updateBindingOptions: function(sCollectionId, oBindingData, sSourceId) {
+			this.mBindingOptions = this.mBindingOptions || {};
+			this.mBindingOptions[sCollectionId] = this.mBindingOptions[sCollectionId] || {};
+
+			var aSorters = this.mBindingOptions[sCollectionId].sorters;
+			var aGroupby = this.mBindingOptions[sCollectionId].groupby;
+
+			// If there is no oBindingData parameter, we just need the processed filters and sorters from this function
+			if (oBindingData) {
+				if (oBindingData.sorters) {
+					aSorters = oBindingData.sorters;
+				}
+				if (oBindingData.groupby || oBindingData.groupby === null) {
+					aGroupby = oBindingData.groupby;
+				}
+				// 1) Update the filters map for the given collection and source
+				this.mBindingOptions[sCollectionId].sorters = aSorters;
+				this.mBindingOptions[sCollectionId].groupby = aGroupby;
+				this.mBindingOptions[sCollectionId].filters = this.mBindingOptions[sCollectionId].filters || {};
+				this.mBindingOptions[sCollectionId].filters[sSourceId] = oBindingData.filters || [];
+			}
+
+			// 2) Reapply all the filters and sorters
+			var aFilters = [];
+			for (var key in this.mBindingOptions[sCollectionId].filters) {
+				aFilters = aFilters.concat(this.mBindingOptions[sCollectionId].filters[key]);
+			}
+
+			// Add the groupby first in the sorters array
+			if (aGroupby) {
+				aSorters = aSorters ? aGroupby.concat(aSorters) : aGroupby;
+			}
+
+			var aFinalFilters = aFilters.length > 0 ? [new sap.ui.model.Filter(aFilters, true)] : undefined;
+			return {
+				filters: aFinalFilters,
+				sorters: aSorters
+			};
+
+		},
+		onInit: function() {
+			this.oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			this.oRouter.getTarget("Page1").attachDisplay(jQuery.proxy(this.handleRouteMatched, this));
+
+		},
+		onExit: function() {
+
+			// to destroy templates for bound aggregations when templateShareable is true on exit to prevent duplicateId issue
+			var aControls = [{
+				"controlId": "sap_Responsive_Page_0-content-sap_m_ObjectList-1593892578068",
+				"groups": ["items"]
+			}];
+			for (var i = 0; i < aControls.length; i++) {
+				var oControl = this.getView().byId(aControls[i].controlId);
+				if (oControl) {
+					for (var j = 0; j < aControls[i].groups.length; j++) {
+						var sAggregationName = aControls[i].groups[j];
+						var oBindingInfo = oControl.getBindingInfo(sAggregationName);
+						if (oBindingInfo) {
+							var oTemplate = oBindingInfo.template;
+							oTemplate.destroy();
+						}
+					}
+				}
+			}
+
+		}
+	});
+}, /* bExport= */ true);
